71dca1583e702333b8e4579855534027ab92b4ba
Emine Gökçe Kekilli <gokcekekilli02@gmail.com>
2026-03-02 16:30:10 +0300

    Time: 45 ms (80.99%), Space: 56.7 MB (53.71%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var curr = init;
+    return {
+        increment:function() {
+            init+=1;
+            return init;
+        },
+        decrement:function() {
+            init-=1;
+            return init;
+        },
+        reset:function() {
+            init = curr;
+            return init;
+        }
+    };
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
