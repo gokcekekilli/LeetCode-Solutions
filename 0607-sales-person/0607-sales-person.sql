6ef6163a583b27c6555681374e95154e19079a2a
Emine Gökçe Kekilli <gokcekekilli02@gmail.com>
2026-08-30 11:21:03 +0300

    Time: 1514 ms (64.73%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT SalesPerson.name
+FROM SalesPerson 
+WHERE sales_id NOT IN (
+    SELECT sales_id
+    FROM Orders
+    JOIN Company
+    ON Orders.com_id = Company.com_id
+    WHERE Company.name = "RED"
+);
+
